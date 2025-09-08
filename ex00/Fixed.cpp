841f529ae61f44267caec1e7e82055b863a1e678
Escudo5 <sergiomargar03@gmail.com>
2025-09-08 13:22:59 +0200

    cambios en cpp y constructor de copia

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: smarquez <smarquez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 10:52:10 by smarquez          #+#    #+#             */
-/*   Updated: 2025/09/08 10:54:09 by smarquez         ###   ########.fr       */
+/*   Updated: 2025/09/08 13:21:42 by smarquez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,30 @@
 Fixed::Fixed()
 {
     std::cout << "Default constructor called" << std::endl;
+    fixedNumberValue = 0;
 }
+Fixed::(const Fixed &copy)
+{
+    std::cout << "Copy constructor called" << std::endl;
+    
+}
+
+Fixed::operator=()
 
 Fixed::~Fixed()
 {
     std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits(void)const
+{
+    std::cout << "getRawBits member function called" << std::endl;
+    return (fixedNumberValue);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+    std::cout << "setRawBits member function called" << std::endl;
+    fixedNumberValue = raw;
 }
 
--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -6,18 +6,22 @@
 /*   By: smarquez <smarquez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 10:47:41 by smarquez          #+#    #+#             */
-/*   Updated: 2025/09/08 10:53:06 by smarquez         ###   ########.fr       */
+/*   Updated: 2025/09/08 13:04:02 by smarquez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FIXED_HPP
+#define FIXED_HPP
 
 #include <iostream>
 
 class  Fixed
 {
     public:
-        Fixed();
-        ~Fixed();
+        Fixed();  //constructor por defecto
+        Fixed(const Fixed &copy); //constructir de copia. //const garantiza que no voy a modificar el objeto original(copy)
+        Fixed &operator=(const Fixed &copy); // operador de asignacion
+        ~Fixed(); //destructor
 
 
         int getRawBits(void) const;
@@ -26,10 +30,12 @@ class  Fixed
 
     private:
         int fixedNumberValue;
-        static const int fractional;
-
-
-
+        static const int fractional = 8;
 
     
-}
+};
+
+#endif
+
+
+// el operador de asignacion devuelve un Clase&, es una referencia a si mismo. 
